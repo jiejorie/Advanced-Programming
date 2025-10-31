e70bd2512f7f1c692dd853a3547e82cac2e55d20
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 18:54:54 +0400

    Update 12-AreaFunction.py
    -Added try-except-else blocks to catch any potential exceptions or errors that may occur

--- Assessment 1/gitHub exercises/Chapter 1/12-AreaFunction.py
+++ Assessment 1/gitHub exercises/Chapter 1/12-AreaFunction.py
@@ -1,26 +1,59 @@
 # EXERCISE 12: AREA FUNCTION
-
 def calculate_area(user_input):
     if user_input == 1:
         print("\nArea of a Square:")
-        side = float(input("Enter a side length for your Square: "))
+        while True:
+            try:
+                side = float(input("Enter a side length for your Square: "))
+            except ValueError:
+                print("Please only enter a digit\n")
+            else:
+                break
         area = side * side
         print(f"\nThe area of your Square is: {area:.2f}!")
     elif user_input == 2:
         print("\nArea of a Circle:")
-        radius = float(input("Enter a radius for your Circle: "))
+        while True:
+            try:
+                radius = float(input("Enter a radius for your Circle: "))
+            except ValueError:
+                print("Please only enter a digit\n")
+            else:
+                break
         area = 3.14 * radius**2
         print(f"\nThe area of your Circle is: {area:.2f}!")
     else:
         print("\nArea of a Triangle:")
-        base = float(input("Enter the base of your Triangle: "))
-        height = float(input("Enter the height of your Triangle: "))
+        
+        while True:
+            try:
+               base = float(input("Enter the base of your Triangle: ")) 
+            except ValueError:
+                print("Please only enter a digit\n")
+            else:
+                break
+        while True:
+            try:
+               height = float(input("Enter the height of your Triangle: "))
+            except ValueError:
+                print("Please only enter a digit\n")
+            else:
+                break
         area = (base * height) / 2
         print(f"\nThe area of your Triangle is: {area:.2f}!")
 
 def menu():
     print("1: Calculate the area of a square\n2: Calculate the area of a circle\n3: Calculate the area of a triangle ")
-    user_input = int(input("\nWhat would you like to calculate?\nType '1', '2', or '3': "))
+    while True:
+        try:
+            user_input = int(input("\nWhat would you like to calculate?\nType '1', '2', or '3': "))
+        except ValueError:
+            print("Please only enter a digit")
+        else:
+            if 1 <= user_input <= 3:
+                break
+            else:
+                print("Please only a number from '1' through '3'")
     return user_input
 
 def main():
