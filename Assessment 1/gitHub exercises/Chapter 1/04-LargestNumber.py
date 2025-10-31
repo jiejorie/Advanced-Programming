be46bd805803995280fd20e9e81af01f8fbddf8e
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 17:26:10 +0400

    Update 04-LargestNumber.py
    -Finalized the if-else statement for the exercise
    -Input type was changed from int to float

--- Assessment 1/gitHub exercises/Chapter 1/04-LargestNumber.py
+++ Assessment 1/gitHub exercises/Chapter 1/04-LargestNumber.py
@@ -1,32 +1,12 @@
 # EXERCISE 4: LARGEST NUMBER
-print("\nLargest Number\n")
-firstNum = int(input("Please enter your first number: \n"))
-secondNum = int(input("Please enter your second number: \n"))
-thirdNum = int(input("Please enter your third number: \n"))
+print("Checking for the Largest Number")
+firstNum = float(input("\nPlease enter your first number: \n"))
+secondNum = float(input("\nPlease enter your second number: \n"))
+thirdNum = float(input("\nPlease enter your third number: \n"))
 
-# if firstNum > secondNum:
-#     if secondNum < thirdNum:
-#         if thirdNum > firstNum:
-#             print(f"{thirdNum} is the Largest Number.")
-#         elif thirdNum < firstNum:
-#             print(f"{firstNum} is the Largest Number.")
-#         else:
-#             print(f"Both {firstNum} and {thirdNum} are the Largest Numbers.")
-#     elif secondNum > thirdNum:
-#         print(f"{firstNum} is the Largest Number.")
-#     else:
-#         print(f"{firstNum} is the Largest Number.")
-# elif secondNum > firstNum:
-#     if firstNum < thirdNum:
-#         if thirdNum > secondNum:
-#             print(f"{thirdNum} is the Largest Number.")
-#         elif thirdNum < secondNum:
-#             print(f"{secondNum} is the Largest Number.")
-#         else:
-#             print(f"Both {secondNum} and {thirdNum} are the Largest Number.")
-#     elif firstNum > thirdNum:
-#         print(f"{secondNum} is the Largest Number.")
-#     else: 
-#         print(f"Both {firstNum} and {thirdNum} are the Largest Numbers.")
-
-    
+if firstNum > secondNum and firstNum > thirdNum:
+    print(f"\nThe largest number is {firstNum}")
+elif secondNum > firstNum and secondNum > thirdNum:
+    print(f"\nThe largest number is {secondNum}")
+else:
+    print(f"\nThe largest number is {thirdNum}")
