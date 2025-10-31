c0c6621e0c9ed2fb56d103e8c7ece60746822afb
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 17:32:59 +0400

    Update 12-AreaFunction.py
    -Exercise 12 answers completed

---
+++ Assessment 1/gitHub exercises/Chapter 1/12-AreaFunction.py
@@ -0,0 +1,41 @@
+# EXERCISE 12: AREA FUNCTION
+
+def square_area():
+    print("\nArea of a Square:")
+    side = float(input("Enter a side length for your Square: "))
+    area = side * side
+    return area
+
+def circle_area():
+    print("\nArea of a Circle:")
+    radius = float(input("Enter a radius for your Circle: "))
+    area = 3.14 * radius**2
+    return area
+
+def triangle_area():
+    print("\nArea of a Triangle:")
+    base = float(input("Enter the base of your Triangle: "))
+    height = float(input("Enter the height of your Triangle: "))
+    area = (base * height) / 2
+    return area
+
+def menu():
+    print("1: Calculate the area of a square\n2: Calculate the area of a circle\n3: Calculate the area of a triangle ")
+    user_input = int(input("\nWhat would you like to calculate?\nType '1', '2', or '3': "))
+    return user_input
+
+def main():
+    print("Calculating the area")
+    user_input = menu()
+    if user_input == 1:
+        area = square_area()
+        print(f"\nThe area of your Square is: {area:.2f}!")
+    elif user_input == 2:
+        area = circle_area()
+        print(f"\nThe area of your Circle is: {area:.2f}!")
+    else:
+        area = triangle_area()
+        print(f"\nThe area of your Triangle is: {area:.2f}!")
+
+if __name__ == "__main__":
+    main()
