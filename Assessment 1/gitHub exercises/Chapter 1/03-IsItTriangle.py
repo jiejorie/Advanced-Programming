2252d31334ed5f2050f8293e79b18f1e3d2ab3d7
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 19:19:52 +0400

    Update 03-IsItTriangle.py
    -Simplified operation using an if-elif-else statement to cover any conditions

--- Assessment 1/gitHub exercises/Chapter 1/03-IsItTriangle.py
+++ Assessment 1/gitHub exercises/Chapter 1/03-IsItTriangle.py
@@ -1,32 +1,17 @@
 # EXERCISE 3: IS IT TRIANGLE
 import random
 print("Is it a triangle?")
-side1 = float(input("Enter the first side of your triangle: \n"))
+side1 = float(input("\nEnter the first side of your triangle: \n"))
 side2 = float(input("Enter the second side of your triangle: \n"))
 side3 = float(input("Enter the third side of your triangle: \n"))
 
-answers = side1, side2, side3
-answersList = list(answers)
-
-def pick_side():
-    side = random.choice(answerslist)
-    answersList.remove(side)
-    return side
-
-firstSide = pick_side()
-secondSide = pick_side()
-thirdSide = pick_side()
-
-randomSides = firstSide, secondSide, thirdSide
-duplicates = [item for item in randomSides if randomSides.count(item) >= 2]
-
-if firstSide + secondSide >= thirdSide:
+if side1 + side2 >= side3 or side2 + side3 >= side1 or side1 + side3 >= side2:
     print("\nThis is indeed a Triangle!")
-    if len(duplicates) == 0:
-        print("\nClassification:\n This Triangle is a Scalene.")
-    elif len(duplicates) == 3:
+    if side1 == side2 == side3:
         print("\nClassification:\n This Triangle is an Equilateral.")
-    else:
+    elif side1 == side2 or side2 == side3 or side1 == side3:
         print("\nClassification:\n This Triangle is an Isosceles.") 
+    else:
+        print("\nClassification:\n This Triangle is a Scalene.")
 else:
     print("\nThis is unfortunately not a Triangle..")
