fe85147b721efba2e30a12ef20b273f456588e4c
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 19:05:53 +0400

    Update 02-Maths.py
    -Added try-except-else block to accept only numbers to calculate the needed equations

--- Assessment 1/gitHub exercises/Chapter 1/02-Maths.py
+++ Assessment 1/gitHub exercises/Chapter 1/02-Maths.py
@@ -1,7 +1,19 @@
 # EXERCISE 2 : MATHS
 print("Maths")
-value1 = float(input("\nEnter your first value: \n"))
-value2 = float(input("Enter your second value: \n"))
+while True:
+    try:
+        value1 = float(input("\nEnter your first value: \n"))
+    except ValueError:
+     print("Error: Please only enter a number\n")
+    else:
+       break
+while True:
+    try:
+        value2 = float(input("Enter your second value: \n"))
+    except ValueError:
+     print("Error: Please only enter a number\n")
+    else:
+       break
 sum = value1 + value2
 diff = value1 - value2
 product = value1 * value2
