0775cafd0a49f8d83752970905f8d0e04b645385
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 19:24:34 +0400

    Update 03-IsItTriangle.py
    -Removed import random
    -Implemented try-except-else blocks to avoid exceptions interrupting the code

--- Assessment 1/gitHub exercises/Chapter 1/03-IsItTriangle.py
+++ Assessment 1/gitHub exercises/Chapter 1/03-IsItTriangle.py
@@ -1,9 +1,26 @@
 # EXERCISE 3: IS IT TRIANGLE
-import random
 print("Is it a triangle?")
-side1 = float(input("\nEnter the first side of your triangle: \n"))
-side2 = float(input("Enter the second side of your triangle: \n"))
-side3 = float(input("Enter the third side of your triangle: \n"))
+while True:
+    try:
+        side1 = float(input("\nEnter the first side of your triangle: \n"))
+    except ValueError:
+        print("Error: Please only enter a number!\n")
+    else:
+        break
+while True:
+    try:
+        side2 = float(input("Enter the second side of your triangle: \n"))
+    except ValueError:
+        print("Error: Please only enter a number!\n")
+    else:
+        break
+while True:
+    try:
+        side3 = float(input("Enter the third side of your triangle: \n"))
+    except ValueError:
+        print("Error: Please only enter a number!\n")
+    else:
+        break
 
 if side1 + side2 >= side3 or side2 + side3 >= side1 or side1 + side3 >= side2:
     print("\nThis is indeed a Triangle!")
