4d0d70ae011f04b8340c1a913d8e2af8224e24f7
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 17:28:01 +0400

    Update 06-FizzBuzz.py
    -Exercise 6 answers submitted

---
+++ Assessment 1/gitHub exercises/Chapter 1/06-FizzBuzz.py
@@ -0,0 +1,11 @@
+# EXERCISE 6: FIZZBUZZ
+print("FizzBuzz Numbers")
+for num in range(1,101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0 :
+        print("Fizz")
+    else:
+        print(num)
