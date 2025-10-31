3523e980e98812468b90e4e24b35720360ed8d84
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 18:58:49 +0400

    Update 01-UserInputOutput.py
    -Fixed the break placement within the try-except block, using else:

--- Assessment 1/gitHub exercises/Chapter 1/01-UserInputOutput.py
+++ Assessment 1/gitHub exercises/Chapter 1/01-UserInputOutput.py
@@ -4,9 +4,11 @@ userName = input("What is your name?\n").title()
 while True:
     try:
         userAge = int(input("What is your age?\n"))
-        break
     except ValueError:
      print("Error: Please enter a number for your age.\n")
+    else:
+        break
 print(f"It is good to meet you, {userName}")
 print(f"The length of your name is:\n{len(userName)}")
+
 print(f"You will be {userAge+1} in a year.")
