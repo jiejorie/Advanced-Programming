f983d226a9ce9822c5e37b16f1a39b2a14bed149
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 17:31:17 +0400

    Update 10-FilmDictionary.py
    -Exercise 10 answers submitted

---
+++ Assessment 1/gitHub exercises/Chapter 1/10-FilmDictionary.py
@@ -0,0 +1,5 @@
+# EXERCISE 10: FILM DICTIONARY
+print("Film Dictionary")
+film_dict = {"Title":"My Neighbor Totoro", "Director": "Hayao Miyazaki", "Release Date": "April 16, 1988", "Running Time": "1 hour and 26 mins"}
+for key, value in film_dict.items():
+    print(f"\n{key}: {value}")
