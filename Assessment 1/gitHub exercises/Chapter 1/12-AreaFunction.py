24ab061d077ce39a5c7f770639a05418538073f0
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-10-31 18:40:47 +0400

    Update 12-AreaFunction.py
    -Combined area calculation functions into one function using an if-elif-else statement

--- Assessment 1/gitHub exercises/Chapter 1/12-AreaFunction.py
+++ Assessment 1/gitHub exercises/Chapter 1/12-AreaFunction.py
@@ -1,23 +1,22 @@
 # EXERCISE 12: AREA FUNCTION
 
-def square_area():
-    print("\nArea of a Square:")
-    side = float(input("Enter a side length for your Square: "))
-    area = side * side
-    return area
-
-def circle_area():
-    print("\nArea of a Circle:")
-    radius = float(input("Enter a radius for your Circle: "))
-    area = 3.14 * radius**2
-    return area
-
-def triangle_area():
-    print("\nArea of a Triangle:")
-    base = float(input("Enter the base of your Triangle: "))
-    height = float(input("Enter the height of your Triangle: "))
-    area = (base * height) / 2
-    return area
+def calculate_area(user_input):
+    if user_input == 1:
+        print("\nArea of a Square:")
+        side = float(input("Enter a side length for your Square: "))
+        area = side * side
+        print(f"\nThe area of your Square is: {area:.2f}!")
+    elif user_input == 2:
+        print("\nArea of a Circle:")
+        radius = float(input("Enter a radius for your Circle: "))
+        area = 3.14 * radius**2
+        print(f"\nThe area of your Circle is: {area:.2f}!")
+    else:
+        print("\nArea of a Triangle:")
+        base = float(input("Enter the base of your Triangle: "))
+        height = float(input("Enter the height of your Triangle: "))
+        area = (base * height) / 2
+        print(f"\nThe area of your Triangle is: {area:.2f}!")
 
 def menu():
     print("1: Calculate the area of a square\n2: Calculate the area of a circle\n3: Calculate the area of a triangle ")
@@ -27,15 +26,8 @@ def menu():
 def main():
     print("Calculating the area")
     user_input = menu()
-    if user_input == 1:
-        area = square_area()
-        print(f"\nThe area of your Square is: {area:.2f}!")
-    elif user_input == 2:
-        area = circle_area()
-        print(f"\nThe area of your Circle is: {area:.2f}!")
-    else:
-        area = triangle_area()
-        print(f"\nThe area of your Triangle is: {area:.2f}!")
+    calculate_area(user_input)
+
 
 if __name__ == "__main__":
     main()
