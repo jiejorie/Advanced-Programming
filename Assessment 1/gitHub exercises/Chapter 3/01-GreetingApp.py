f554d86a50c460d8ff64cb70317844a5e090f88e
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-11-10 18:52:37 +0400

    Update 01-GreetingApp.py
    Added comments for descriptive purposes

--- Assessment 1/gitHub exercises/Chapter 3/01-GreetingApp.py
+++ Assessment 1/gitHub exercises/Chapter 3/01-GreetingApp.py
@@ -1,10 +1,12 @@
 from tkinter import*
 from tkinter import ttk
 
+# calls mainloop() if called
 def main():
     gui = MyGUI()
     gui.mainloop()
 
+# holds the primary GUI
 class MyGUI(Tk):
     def __init__(self):
         super().__init__()
@@ -12,49 +14,59 @@ class MyGUI(Tk):
         self.geometry("400x400")
         self.maxsize(400, 400)
 
+        # widgets
+        # Input Frame
         self.InputFrame = InputFrame(self)
         self.InputFrame.pack(side="top", fill="both", expand=True, anchor="n")
-        
+        # Display Frame
         self.DisplayFrame = DisplayFrame(self)
         self.DisplayFrame.pack(side="bottom", fill="both", expand=True, anchor="s")
 
+    # updates greeting message based on entry and option menu input
     def update_greeting(self):
         self.name = self.InputFrame.username.get().title()
         self.value = self.InputFrame.value.get()
         self.DisplayFrame.greeting.configure(text=f"Greetings, {self.name}!", fg=self.value)
 
         
-
+# holds the Input Frame widgets
 class InputFrame(Frame):
+    # allows inheritance
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
         self.config(bg="#FFFF94")
 
+        # widgets
+        # 'what is your name?' text
         self.title_lbl = Label(self, text="What is your name?", font=("Segoe UI", 20, "bold"), fg="blue", bg="#FFFF94").pack(side="top", pady=20)
-
+        # username entry
         self.username = Entry(self, width=28, font=("Segoe UI", 10), relief="flat", bd=10)
         self.username.pack(side="top", pady=5)
-
+        # initializes variables for option menu widget
         self.color_options = ["Red", "Orange", "Gold", "Dark Green", "Blue",  "Purple", "Deep Pink", "Black"]
         self.value = StringVar(self)
         self.value.set("Blue")
-
+        # color options 
         self.color_menu = OptionMenu(self, self.value, *self.color_options)
         self.color_menu.pack(pady=10)
-
         self.color_menu.config(width=26, height=2, bd=0, bg="blue", fg="white", font=("Segoe UI", 10))
-
+        # update greeting button
         self.update = Button(self, text="Update greeting", font=("Segoe UI", 10), fg="white", bg="blue", bd=0, width=30, command=self.parent.update_greeting).pack(pady=15, ipady=5)
 
+# holds the Display frame widgets
 class DisplayFrame(Frame):
+    # allows inheritance
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
         self.config(bg="#ADFFD6")
 
+        # widgets
+        # greeting message
         self.greeting = Label(self, text="", font=("Segoe UI", 20, "bold"), bg="#ADFFD6")
         self.greeting.pack(pady=20, anchor="center", side="top")
 
+# calls main if script is being directly run
 if __name__ == "__main__":
     main()
