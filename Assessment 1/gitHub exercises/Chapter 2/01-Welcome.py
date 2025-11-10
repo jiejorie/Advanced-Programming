c0cc4fe86cbe936cd8fc51d96c71b0943615e433
jiejorie <168099968+jiejorie@users.noreply.github.com>
2025-11-10 18:30:33 +0400

    Update 01-Welcome.py

--- Assessment 1/gitHub exercises/Chapter 2/01-Welcome.py
+++ Assessment 1/gitHub exercises/Chapter 2/01-Welcome.py
@@ -1,10 +1,12 @@
 from tkinter import*
 from tkinter import ttk
 
+# runs mainloop() when called
 def main():
     gui = MyGUI()
     gui.mainloop()
 
+# holds primary GUI
 class MyGUI(Tk):
     def __init__(self):
         super().__init__()
@@ -14,12 +16,14 @@ class MyGUI(Tk):
         self.maxsize(300, 300)
         self.configure(background="black")
 
-
+        # Styles Label using ttk
         self.style = ttk.Style()
         self.style.configure("label.TLabel", font=("Segoe UI", 25, "bold"), foreground="white", background="black")
 
+        # widgets
         self.label = ttk.Label(self, text="Welcome", style="label.TLabel")
         self.label.pack(pady=50)
 
+# calls main() if script is directly being ran
 if __name__ == "__main__":
     main()
